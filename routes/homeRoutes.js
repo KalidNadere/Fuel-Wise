b598dd0e65804f61a6f2f45bc49109f7b5e5b61d
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-08-28 20:31:15 +1000

    Fixed server issues and moved login and signup handlebars out of partials folder.

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 
 router.get('/',async (req,res) => {
-    res.render('dashboard');
+    try{
+        console.log('Render homepage')
+        res.render('dashboard');
+    }catch{
+        console.log("didnt render")
+    }
+    
 })
 
-router.get('/', async (req,res) => {
+router.get('/login', async (req,res) => {
     res.render('login')
 })
 
-router.get('/',async (req,res) => {
+router.get('/signup',async (req,res) => {
     res.render('signup')
 })
 
--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const app = express();
 
 const hbs = exphbs.create({});
 
-// Set up middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
-
 // Set up view engine (Handlebars)
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 // app.set('views', path.join(__dirname, 'views'));
 
+// Set up middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
+
 // Define routes
-app.use('/routes', routes);
+app.use(routes);
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
