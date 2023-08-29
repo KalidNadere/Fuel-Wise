6b8db665cbeeb2c8fcf7673a232ebeb1f195d86c
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-08-29 19:55:35 +1000

    Added code to login route to redirect back to homepage if already logged in

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -11,7 +11,13 @@ router.get('/',async (req,res) => {
 })
 
 router.get('/login', async (req,res) => {
-    res.render('login')
+    // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 })
 
 module.exports = router;
