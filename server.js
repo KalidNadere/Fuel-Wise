4e22cefd1838d399956659514862767eab893893
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-08-29 19:10:24 +1000

    resolved merge conflict

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
 
-// Set up view engine (Handlebars)
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
-
 // Define routes
 app.use(routes);
 
