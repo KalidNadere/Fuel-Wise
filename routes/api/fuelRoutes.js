b7b061dd0202316320558e42e24381f5badb898e
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-08-31 21:16:17 +1000

    Added code for fueRoutes

---
+++ routes/api/fuelRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const {FuelStation} = require('../../models')
+
+const fuelController = require('../controllers/fuelController');
+const isAuthenticated = require('../../middlewares/authMiddleware')
+
+router.get('/locations', async (req,res) =>{
+    try{
+        const stationData = await FuelStation.findAll()
+        stationData = JSON.stringify(stationData)
+        res.status(200).json(stationData);
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// Define fuel-related routes
+router.get('/prices', fuelController.showFuelPrices);
+
+module.exports = router;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
+// const showFuelPrices = require('../controllers/fuelController');
+const axios = require('axios');
+require('dotenv').config();
 
 router.get('/',async (req,res) => {
     try{
-        console.log('Render homepage')
+        const stationPrices = showFuelPrices();
         const logged_in = req.session.logged_in || false;
-        res.render('dashboard', { logged_in }); 
-    }catch{
+        res.render('dashboard', {
+            ...stationPrices,
+            logged_in
+        });
+        console.log('Rendered homepage')
+    }catch (err) {
         console.log("didnt render")
+        res.status(500).json(err)
     }
-    
 })
 
 router.get('/login', async (req,res) => {
@@ -17,8 +24,71 @@ router.get('/login', async (req,res) => {
         res.redirect('/');
         return;
     }
-
     res.render('login');
 })
+
+// // Fetch nearby fuel stations and prices based on user's geolocation
+// const fetchNearbyfuelStations = async (lat, lon) => {
+//     const url = "https://api.onegov.nsw.gov.au/FuelPriceCheck/v1/fuel/prices/nearby";
+//     const headers = {
+//       "accept": "application/json",
+//       "Authorization": process.env.ACCESS_KEY,
+//       "Content-Type": "application/json",
+//       "apikey": process.env.API_KEY,
+//       "transactionid": "123",
+//       "requesttimestamp": "28/08/2023 07:30:20 PM"
+//     };
+//     const postBody = {
+//       fueltype: "P95", // Fuel type(Examples: P95, P98, E10). REQUIRED
+//       brand: [], // Fuel brand name(Examples: BP, Shell, Caltex). NOT REQUIRED
+//       namedlocation: "", // Suburb Number(Examples: 2000, 2015). NOT REQUIRED
+//       //latitude and longitude. REQUIRED
+//       latitude: lat,
+//       longitude: lon,
+//       radius: "5", // Search radius. REQUIRED
+//       sortby: "price",
+//       sortascending: "true"
+//     };
+//     const options = {
+//       method: 'POST',
+//       headers: headers,
+//       body: JSON.stringify(postBody)
+//     };
+  
+//     try {
+      
+//       const response = await axios.get(url,options);
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error fetching nearby fuel stations:', error);
+//       return [];
+//     }
+// };
+  
+// // Process fuel prices and render view
+// const showFuelPrices = async () => {
+// try {
+//     // Get user's geolocation
+//     navigator.geolocation.getCurrentPosition(
+//     async (position) => {
+//         const lat = position.coords.latitude;
+//         const lon = position.coords.longitude;
+
+//         // Fetch fuel prices and nearby fuelstations
+//         const nearbyStationPrices = await fetchNearbyfuelStations(lat, lon);
+
+//         // Render view with fuel prices and nearby fuel stations
+//         return nearbyStationPrices;
+//     },
+//     (error) => {
+//         console.error('Geolocation error:', error);
+//         return error;
+//     }
+//     );
+// } catch (error) {
+//     console.error('Error processing fuel prices:', error);
+//     res.status(500).send('Internal server error');
+// }
+// };
 
 module.exports = router;
