d8aba7d7a6b4d9ffd1c762fb8f31a7f980bc09ed
Michael King <2017tempking@gmail.com>
2023-09-02 19:50:14 +1000

    Updated to use the API from the client side

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,50 +1,36 @@
+// Declare global variables
+let map;
+let pos;
+let dataNSW;
+
+// Navigator requests Geolocation
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    console.log(pos);
+    map.setCenter(pos);
+    getFuel();
+  },
+  () => {
+    handleLocationError(true, infoWindow, map.getCenter());
+  },
+);
+
+// Google Maps
 async function initMap() {
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   const { LatLng } = await google.maps.importLibrary("core");
-  const center = new LatLng(-33.43238031167444, 151.16795397128632);
-  const map = new Map(document.getElementById("map"), {
+  const center = new LatLng(-33.8663965, 151.0467039);
+  map = new Map(document.getElementById("map"), {
     zoom: 11,
     center,
     mapId: "d13ebef7973e4ad2"
   });
-
-  const stations = NSWdata.stations;
-  for (const station of stations) {
-    const lat = station.location.latitude;
-    const lng = station.location.longitude;
-
-    const AdvancedMarkerElement = new google.maps.marker.AdvancedMarkerElement({
-      map,
-      content: newStation(station),
-      position: {lat, lng},
-      title: station.name,
-    });
-
-    AdvancedMarkerElement.addListener("click", () => {
-      toggleHighlight(AdvancedMarkerElement, station);
-    });
-  }
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        console.log(pos);
-        map.setCenter(pos);
-      },
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      },
-    );
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
 }
 
 function toggleHighlight(markerView, property) {
@@ -62,118 +48,83 @@ function newStation(station) {
 
   content.classList.add("station");
   content.innerHTML = `
-      <div class="icon">
-          <span>${station.prices.fueltype} ${station.prices.price}c</span>
-      </div>
-      <div class="details">
-          <div class="name">${station.name}</div>
-          <div class="address">${station.address}</div>
-          <div class="distance">${station.location.distance}km away</div>
-          <div class="prices">
-              <div class="price">${station.prices.price}c - ${station.prices.fueltype}</div>
-              <div class="price">${station.prices.lastupdated}</div>
-          </div>
-      </div>
-      `;
+    <div class="icon">
+        <span>${station.prices.price}c \u00A0</span>
+        <span class="cent">${station.brand}</span>
+    </div>
+    <div class="details">
+    <div class="prices">
+        <div class="price">${station.prices.price}<span class="cent">c - </span>${station.prices.fueltype}</div>
+        <div class="lastUpdated">${station.prices.lastupdated}</div>
+    </div>
+        <div class="distance">${station.location.distance}<span class="kilo"> km away</span></div>
+        <div class="name">${station.name}</div>
+        <div class="address">${station.address}</div>
+
+    </div>
+    `;
   return content;
 }
 
-const NSWdata = {
-  "stations": [
-      {
-      "stationid": "1-GN9E-471",
-      "brandid": "1-GFYV-0",
-      "brand": "BP",
-      "code": 415,
-      "name": "BP Tumbi Umbi",
-      "address": "Cnr Wyong Rd & Mingara Dve, Tumbi Umbi NSW 2261",
-      "location": {
-          "latitude": -33.36272,
-          "longitude": 151.44428,
-          "distance": 15.97
-      }
-      },
-      {
-      "stationid": "1-GN9E-401",
-      "brandid": "1-GFYV-0",
-      "brand": "BP",
-      "code": 338,
-      "name": "BP Berkeley Vale",
-      "address": "1 Blade Close, Berkeley Vale NSW 2261",
-      "location": {
-          "latitude": -33.330562,
-          "longitude": 151.423842,
-          "distance": 16.66
-      }
-      },
-      {
-      "stationid": "1-GN9E-441",
-      "brandid": "1-GFYV-0",
-      "brand": "BP",
-      "code": 382,
-      "name": "BP Wyoming",
-      "address": "481 Pacific Highway, Wyoming NSW 2250",
-      "location": {
-          "latitude": -33.408004,
-          "longitude": 151.351119,
-          "distance": 5.96
-      }
-      },
-      {
-      "stationid": "1-3VXN3NQ",
-      "brandid": "1-GFYV-0",
-      "brand": "BP",
-      "code": 19016,
-      "name": "BP Erina",
-      "address": "90 THE ENTRANCE ROAD, ERINA NSW 2250",
-      "location": {
-          "latitude": -33.438429,
-          "longitude": 151.372213,
-          "distance": 7.03
-      }
-      }
-  ],
-  "prices": [
-      {
-      "stationcode": 338,
-      "fueltype": "P95",
-      "price": 220.9,
-      "priceunit": "litre",
-      "description": null,
-      "lastupdated": "2023-08-23 00:28:45"
-      },
-      {
-      "stationcode": 382,
-      "fueltype": "P95",
-      "price": 229.9,
-      "priceunit": "litre",
-      "description": null,
-      "lastupdated": "2023-08-23 00:28:45"
-      },
-      {
-      "stationcode": 415,
-      "fueltype": "P95",
-      "price": 211.9,
-      "priceunit": "litre",
-      "description": null,
-      "lastupdated": "2023-08-23 05:22:49"
-      },
-      {
-      "stationcode": 19016,
-      "fueltype": "P95",
-      "price": 235.9,
-      "priceunit": "litre",
-      "description": null,
-      "lastupdated": "2023-08-23 19:52:52"
-      }
-  ]
+async function getFuel() {
+    let lat = pos.lat;
+    let lon = pos.lng;
+    const apiFetchPrice = (lat,lon) => {
+      //api URL
+      const url = "https://api.onegov.nsw.gov.au/FuelPriceCheck/v1/fuel/prices/nearby";
+      const headers = {
+          "accept": "application/json",
+          "Authorization": "Bearer 2hrpGYsaWHhMJUTa2RxEl3uEYGu9",//process.env.ACCESS_KEY,
+          "Content-Type": "application/json",
+          "apikey": "csnAKudd1pOGKLWIikPZRbZe9YrnO7Ij",//process.env.API_KEY,
+          "transactionid": "123",
+          "requesttimestamp": "28/08/2023 07:30:20 PM"
+      };
+      //necessary data to input for fetch request
+      const postBody = {
+          //full type necessary
+          fueltype: "P95",
+          brand: [],
+          namedlocation: "",
+          //latitude and longitude are necessary
+          latitude: lat,
+          longitude: lon,
+          radius: "5",
+          sortby: "price",
+          sortascending: "true"
+      };
+      const options = {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify(postBody)
+      };
+      fetch(url, options)
+      .then(response => response.json())
+      .then(data => updateMap(data))
+      .catch(error => console.error('Error:', error));
+    };
+    const runAPI = await apiFetchPrice(pos.lat, pos.lng)
 }
 
-for (const station of NSWdata.stations) {
-  console.log(station);
-  let obj = NSWdata.prices.find(obj => obj.stationcode === station.code);
-  console.log(obj);
-  station.prices = obj;
+function updateMap(data) {
+  console.log(data);
+  for (const station of data.stations) {
+    let obj = data.prices.find(obj => obj.stationcode === station.code);
+    station.prices = obj;
+    const lat = station.location.latitude;
+    const lng = station.location.longitude;
+    const AdvancedMarkerElement = new google.maps.marker.AdvancedMarkerElement({
+      map,
+      content: newStation(station),
+      position: {lat, lng},
+      title: station.name,
+    });
+  
+    AdvancedMarkerElement.addListener("click", () => {
+      toggleHighlight(AdvancedMarkerElement, station);
+    });
+  }
+
 }
 
 initMap();
